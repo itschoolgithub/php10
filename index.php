f3b030a8b847e03e0759403cbf5d21b6aec92801
itschool <itschool@tou.edu.kz>
2024-11-05 00:31:44 +0500

    init

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+    // $string = 'Hello World!';
+    // $string = 'Привет, Мир!';
+    // $string[0] = 'P';
+    // echo mb_strlen($string);
+    // echo $string;
+
+    // $string = "Привет, Мир!";
+    // echo mb_strtoupper($string);
+
+    // $string = "Привет, Мир!";
+    // echo mb_strtolower($string);
+
+    // $string = "7 4 2 8 10";
+    // $numbers = explode(" ", $string);
+    // print_r($numbers);
+
+    // $names = ["John", "Max", "Bob"];
+    // $names = implode("; ", $names);
+    // $names = explode("; ", $names);
+    // print_r($names);
+
+    // $string = "abcdef";
+    // $symbols = str_split($string, 3);
+    // print_r($symbols);
+
+    // echo ltrim("*Hello*", "*");
+    // echo trim("***World***", "*");
+    // echo rtrim("*Hello*", "*");
+
+    // echo substr("abcdef", 1, 3);
+    // echo mb_substr("абвгдеж", 0, -3);
